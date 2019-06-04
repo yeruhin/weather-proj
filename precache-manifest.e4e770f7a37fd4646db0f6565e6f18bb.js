76edfd26df9baa3b1a4b30225ad9431a265f2047
yeruhin <yeruhin@gmail.com>
2019-06-04 18:52:54 +0300

    Updates

---
+++ precache-manifest.e4e770f7a37fd4646db0f6565e6f18bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79658b3e280468fbda1009779296444c",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "7f5fcb4c6e48b8d13828",
+    "url": "/repository-name/static/css/main.57de1450.chunk.css"
+  },
+  {
+    "revision": "70e0d296c53c25d9ab50",
+    "url": "/repository-name/static/js/2.95558ac9.chunk.js"
+  },
+  {
+    "revision": "7f5fcb4c6e48b8d13828",
+    "url": "/repository-name/static/js/main.8d0d42a8.chunk.js"
+  },
+  {
+    "revision": "6e2fc4b7d77a9c53df47",
+    "url": "/repository-name/static/js/runtime~main.f7634681.js"
+  },
+  {
+    "revision": "fc2321639940ae82b8b1380fa8441417",
+    "url": "/repository-name/static/media/wp2895397.fc232163.jpg"
+  }
+]);
